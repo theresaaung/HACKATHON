2c26ddfbbe6591480b6a79f1c14931121524fe97
JamesSknr <8jamesskinner8@gmail.com>
2025-10-25 14:43:07 +0100

    adding sliders

---
+++ company-net-zero-sim/simulation.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var packaging_slider: VBoxContainer = $PackagingSlider
+
+
+# Slider variables
+var eco_packaging: int
+var eco_recycling: int
+var eco_food_import: int
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	packaging_slider.set_label("Eco Packaging")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
