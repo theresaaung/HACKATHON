5d1af4e70a888530ffa5a69a129888bd53aa6c6c
Theresa <3038714W@student.gla.ac.uk>
2025-10-25 21:44:55 +0100

    added setting panel

--- company-net-zero-sim/Menu/main_menu.gd
+++ company-net-zero-sim/Menu/main_menu.gd
@@ -1,23 +1,28 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+@onready var main_buttons: VBoxContainer = $MainButtons
+@onready var settings: Panel = $Settings
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _process(delta) -> void:
+	pass
 
+func _ready():
+	main_buttons.visible = true
+	settings.visible = false
 
 func _on_startbutton_pressed() -> void:
 	get_tree().change_scene_to_file("res://Menu/intro.tscn")
 
 
 func _on_settingsbutton_pressed() -> void:
-	pass # Replace with function body.
+	main_buttons.visible = false
+	settings.visible = true
 
 
 func _on_exitbutton_pressed() -> void:
 	get_tree().quit()
+
+func _on_back_pressed() -> void:
+	settings.visible = false
+	main_buttons.visible = true
